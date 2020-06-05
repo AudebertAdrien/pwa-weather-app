313c3f60cb626fe2757b6e5c0282edb9509db386
AudebertAdrien <audebertadrien.pro@gmail.com>
2020-06-05 18:29:43 +0200

    pwa optimize

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,16 @@ function App() {
   const search = async (e) => {
     if (e.key === "Enter") {
       const data = await fetchWeather(query);
-      console.log(data)
       setWeather(data);
-      setQuery("")
+      setQuery("");
     }
   };
 
   return (
     <div className="main-container">
+      <label htmlFor="place">Place</label>
       <input
+        id="place "
         type="text"
         className="search"
         placeholder="Search..."
@@ -27,19 +28,23 @@ function App() {
       />
       {weather.main && (
         <div className="city">
-            <h2 className="city-name">
-                <span>{weather.name}</span>
-                <sup>{weather.sys.country}</sup>
-            </h2>
-            <div className="city-temp">
-                {Math.round(weather.main.temp)}
-                <sup>&deg;C</sup>
-            </div>
-            <div className="info">
-                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
-                <p>{weather.weather[0].description}</p>
-            </div>
-        </div> 
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
       )}
     </div>
   );
